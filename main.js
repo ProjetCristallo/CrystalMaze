bb30718ec8239f7127ed476e9946840884d9fc0f
Thomas Coeffic <thomas.coeffic@ensimag.grenoble-inp.fr>
2014-05-28 09:11:35 +0200

    Ajout des groupes pour les blocs.
    Avec les groupes du parser uniquement.

--- main.js
+++ main.js
@@ -14,6 +14,16 @@ var playing=true;
 //variable indicating what the last direction taken was.
 var lastDir;
 
+// Blocks groups
+var Hole = group();
+var Simple = group();
+var Unilateral = group();
+var Fragile = group();
+var C_up = group();
+var C_down = group();
+var C_left = group();
+var C_right = group();
+
 
 function preload() {
 	game.load.image('logo','ressources/Bille.png');
@@ -85,6 +95,12 @@ function moveBall() {
 		game.physics.arcade.collide(Ball, normalBlocks, normalBlockCollide, null, this);
 		game.physics.arcade.collide(Ball, breakableBlocks, breakBlockCollide, null, this);
 		game.physics.arcade.overlap(Ball, endBlocks, endLevel, null, this);
+		game.physics.arcade.overlap(Ball, C_up, changeUp, null, this);
+		game.physics.arcade.overlap(Ball, C_down, changeDown, null, this);
+		game.physics.arcade.overlap(Ball, C_right, changeRight, null, this);
+		game.physics.arcade.overlap(Ball, C_left, changeLeft, null, this);
+		game.physics.arcade.overlap(Ball, Hole, holeOverlap, null, this);
+		game.physics.arcade.collide(Ball, Fragile, fragileCollide, null, this);
 	}	
 }
 
@@ -135,6 +151,31 @@ function normalBlockCollide()
 {
 }
 
+function changeUp()
+{
+}
+
+function changeDown()
+{
+}
+
+function changeRight()
+{
+}
+
+function changeLeft()
+{
+}
+
+function holeOverlap()
+{
+}
+
+function fragileCollide()
+{
+}
+
+
 function breakBlockCollide(Ball, breakBlock)
 {
 	breakBlock.damage(1);
@@ -142,87 +183,85 @@ function breakBlockCollide(Ball, breakBlock)
 		lastDir=null;
 	}   
 }
-/*
-   function parser(filename) {
-   var file = new ActiveXObject("Scripting.FileSystemObject");
-   var f_in = file.OpenTextFile(filename,1);
-   while (!f_in.AtEndOfStream) {
-   var block;
-   var line = f_in.ReadLine();
-   var res = line.split(" ");
-   x = 30+60*parseInt(res[1]);
-   y = 30+60*parseInt(res[2]);
-   switch(res[0]) {
-   case "begin":
-   Ball = game.add.sprite(x,y,'logo');
-   Ball.anchor.setTo(0.5,0.5);
-   Ball.checkWorldBounds = true;
-   game.physics.enable(Ball,Phaser.Physics.ARCADE);
-   Ball.body.collideWorldBounds = true;
-   BallMoving = false;
-   break;
-   case "end":
-   break;
-   case "hole":
-   block = Hole.create(x,y,'hole');
-   game.physics.enable(block,Phaser.Physics.ARCADE);
-   block.body.immovable = true;
-   break;
-   case "simple":
-   block = Simple.create(x,y,'simple');
-   game.physics.enable(block,Phaser.Physics.ARCADE);
-   block.body.immovable = true;
-   break;
-   case "unilateral":			
-   switch(res[3]) {
-   case "up":
-   block = Unilateral.create(x,y,'u_up');
-   block.body.checkCollision.up = false;
-   break;
-   case "down":
-   block = Unilateral.create(x,y,'u_d');
-   block.body.checkCollision.down = false;
-   break;
-   case "right":
-   block = Unilateral.create(x,y,'u_r');
-   block.body.checkCollision.right = false;
-   break;
-   case "left":
-   block = Unilateral.create(x,y,'u_l');
-   block.body.checkCollision.left = false;
-   break;
-   }
-   game.physics.enable(block,Phaser.Physics.ARCADE);
-   block.body.immovable = true;
-   break;
-   case "fragile":
-   block = Fragile.create(x,y,'fragile');
-   block.health = parseInt(res[3]);
-   game.physics.enable(block,Phaser.Physics.ARCADE);
-   block.body.immovable = true;
-   break;
-   case "change_up":
-   block = C_up.create(x,y,'c_up');
-   game.physics.enable(block,Phaser.Physics.ARCADE);
-   block.body.immovable = true;
-   break;
-   case "change_down":
-   block = C_down.create(x,y,'c_down');
-   game.physics.enable(block,Phaser.Physics.ARCADE);
-   block.body.immovable = true;
-   break;
-   case "change_right":
-   block = C_right.create(x,y,'c_right');
-game.physics.enable(block,Phaser.Physics.ARCADE);
-block.body.immovable = true;
-break;
-case "change_left":
-block = C_left.create(x,y,'c_left');
-game.physics.enable(block,Phaser.Physics.ARCADE);
-block.body.immovable = true;
-break;
+
+function parser(filename) {
+	var file = new ActiveXObject("Scripting.FileSystemObject");
+	var f_in = file.OpenTextFile(filename,1);
+	while (!f_in.AtEndOfStream) {
+		var block;
+		var line = f_in.ReadLine();
+		var res = line.split(" ");
+		x = 30+60*parseInt(res[1]);
+		y = 30+60*parseInt(res[2]);
+		switch(res[0]) {
+			case "begin":
+				Ball = game.add.sprite(x,y,'logo');
+				Ball.anchor.setTo(0.5,0.5);
+				Ball.checkWorldBounds = true;
+				game.physics.enable(Ball,Phaser.Physics.ARCADE);
+				Ball.body.collideWorldBounds = true;
+				BallMoving = false;
+				break;
+			case "end":
+				break;
+			case "hole":
+				block = Hole.create(x,y,'hole');
+				game.physics.enable(block,Phaser.Physics.ARCADE);
+				block.body.immovable = true;
+				break;
+			case "simple":
+				block = Simple.create(x,y,'simple');
+				game.physics.enable(block,Phaser.Physics.ARCADE);
+				block.body.immovable = true;
+				break;
+			case "unilateral":			
+				switch(res[3]) {
+					case "up":
+						block = Unilateral.create(x,y,'u_up');
+						block.body.checkCollision.up = false;
+						break;
+					case "down":
+						block = Unilateral.create(x,y,'u_d');
+						block.body.checkCollision.down = false;
+						break;
+					case "right":
+						block = Unilateral.create(x,y,'u_r');
+						block.body.checkCollision.right = false;
+						break;
+					case "left":
+						block = Unilateral.create(x,y,'u_l');
+						block.body.checkCollision.left = false;
+						break;
+				}
+				game.physics.enable(block,Phaser.Physics.ARCADE);
+				block.body.immovable = true;
+				break;
+			case "fragile":
+				block = Fragile.create(x,y,'fragile');
+				block.health = parseInt(res[3]);
+				game.physics.enable(block,Phaser.Physics.ARCADE);
+				block.body.immovable = true;
+				break;
+			case "change_up":
+				block = C_up.create(x,y,'c_up');
+				game.physics.enable(block,Phaser.Physics.ARCADE);
+				block.body.immovable = true;
+				break;
+			case "change_down":
+				block = C_down.create(x,y,'c_down');
+				game.physics.enable(block,Phaser.Physics.ARCADE);
+				block.body.immovable = true;
+				break;
+			case "change_right":
+				block = C_right.create(x,y,'c_right');
+				game.physics.enable(block,Phaser.Physics.ARCADE);
+				block.body.immovable = true;
+				break;
+			case "change_left":
+				block = C_left.create(x,y,'c_left');
+				game.physics.enable(block,Phaser.Physics.ARCADE);
+				block.body.immovable = true;
+				break;
+		}
+	}
 }
-}
-}
-}
-*/
