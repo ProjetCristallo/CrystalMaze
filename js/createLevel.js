f403a2061bf319187a8cdeb1e060633ac58e64c4
Dimitri Bouleau <Dimitri.Bouleau@ensimag.imag.fr>
2014-06-16 10:58:13 +0200

    correction erreur

--- js/createLevel.js
+++ js/createLevel.js
@@ -10,5 +10,7 @@ function createLevel()
 	listItem.length = 0;
 	parser(levelName);
 	ball.animations.play("rolling",constants.BALL_ANIMATION_SPEED,true);
+    if (tutorial){
         displayTutorial();
+    }
 }
