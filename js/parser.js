9b5ecb6789c29b79a397386054a99789e819541e
Théo Van-Lede <theo.vanlede@gmail.com>
2014-06-16 15:00:47 +0200

    modifications de parser et ajout de commentaires

--- js/parser.js
+++ js/parser.js
@@ -1,3 +1,6 @@
+/** Parse a file and populate the world accordingly.
+  * @param {string} filename File to load.
+  */
 function parser(filename) {
 	if(document.all) {
 		var file = new ActiveXObject("Scripting.FileSystemObject");
@@ -13,16 +16,26 @@ function parser(filename) {
 	var res = arrLines[0].split(" ");
 	twoStars = res[0];
 	threeStars = res[1];
+	// the block of the current line
+	var block;
+	// the different parameter defining the current block
+	var res;
+	// coordinates of a block
+	var x,y;
 	//We build the level	
 	for(var i = 1 ; i < arrLines.length ; i++) {
-		var block;
-		var line = arrLines[i];
-		var res = line.split(" ");
+		// The previous value are reset
+		block = null;
+		res = null;
+
+		// Loading of the new value
+		res = arrLines[i].split(" ");
 		x = constants.TILE_SIZE*parseInt(res[1]);
 		y = constants.TILE_SIZE*parseInt(res[2]);
+
 		switch(res[0]) {
 			case "begin":
-				addBeginSprite(res[3]);
+				addBeginSprite(res[3],x,y);
 				break;
 			case "end":
 				block = end.create(x,y,'end');
@@ -51,7 +64,7 @@ function parser(filename) {
 				block.type=type;
 				break;
 			case "unilateral":		
-				addUniSprite(res[3]);
+				addUniSprite(res[3],x,y);
 				break;
 			case "salt" : 
 				block = salt.create(x,y,'salt');
@@ -80,13 +93,20 @@ function parser(filename) {
 				block.body.immovable = true;
 				break;
 			case "turn":
-				addTurnSprite(res[3]);
+				addTurnSprite(res[3],x,y);
 				break;
 		}
 	}
 }
 
-function addBeginSprite(state)
+/** Add the sprite corresponding to the position of ball at the beginning of 
+  * the level.
+  * @param {string} state Define the state in which the level starts (must be 
+  * "water", "ice" or "steam").
+  * @param {int} x The x-coordinate of this sprite.
+  * @param {int} y The y-coordinate of this sprite.
+  */
+function addBeginSprite(state,x,y)
 {
 	ball = game.add.sprite(x,y,'ball',4);
 	ball.name = state;
@@ -114,7 +134,13 @@ function addBeginSprite(state)
 	ball.isMoving = false;
 }
 
-function addUniSprite(dir)
+/** Add a sprite corresponding to a unilateral block.
+  * @param {string} dir Define the direction in which the user can get through
+  * this block (must be "up", "down", "left" or "right").
+  * @param {int} x The x-coordinate of this sprite.
+  * @param {int} y The y-coordinate of this sprite.
+  */
+function addUniSprite(dir,x,y)
 {
 	switch(dir) {
 		case "up":
@@ -144,7 +170,14 @@ function addUniSprite(dir)
 	}
 }
 
-function addTurnSprite(orientation)
+/** Add a sprite corresponding to a turn block.
+  * @param {string} orientation Define the directions in which the user can be 
+  * redirected (up and left : "ul", up and right : "ur", down and left :"dl",
+  * down and right : "dr").
+  * @param {int} x The x-coordinate of this sprite.
+  * @param {int} y The y-coordinate of this sprite.
+  */
+function addTurnSprite(orientation,x,y)
 {
 	switch(orientation){
 		case "ul":
